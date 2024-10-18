56b5a01419d291be3e43c6931671b9a4867ff648
Hiroshan Madusanka <pmhiroshan@gmail.com>
2024-10-18 14:41:37 +0530

    DataBase testing

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,23 +1,65 @@
-export function getUsers(req,res){
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        default: "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Save location collection name "Users is database name simple capital not problem"
+const User = mongoose.model("Users", userSchema);
+
+export function getUsers(req, res) {
     res.json({
-        message:"This is get request"
-    })
+        message: "This is a GET request"
+    });
 }
 
-export function postUsers(req,res){
-     res.json({
-        message:"This is post request"
-     })
+export function postUsers(req, res) {
+    const user = req.body;
+
+    // Insert data to DB
+    const newUser = new User(user);
+    newUser.save()
+        .then(() => {
+            res.json({
+                message: "User created successfully."
+            });
+        })
+        .catch((error) => {
+            res.json({
+                message: "User creation failed.",
+                
+            });
+        });
 }
 
-export function putUsers(req,res){
+export function putUsers(req, res) {
     res.json({
-        message:"This is put request"
-    })
+        message: "This is a PUT request"
+    });
 }
 
-export function deleteUsers(req,res){
+export function deleteUsers(req, res) {
     res.json({
-        message:"This is delete request"
-    })
+        message: "This is a DELETE request"
+    });
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import userRoutes from './routes/uersRousts.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,20 @@ app.use(bodyParser.json()) //bodypaerser middlware
 
 //routes
 app.use("/api/users", userRoutes)
+
+const connectionString="mongodb+srv://tester:123@project.rm6md.mongodb.net/?retryWrites=true&w=majority&appName=project"
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Connected to the DataBase.")
+    }
+).catch(
+    ()=>{
+        console.log("Connection Failed.")
+    }
+)
+
+
 
 
 app.listen(5000, () => {
